a93b3d2c0d23a17f3142d24a8163ffdd04e2bf0b
valentina Tangarife <mariav.tangarifev@konradlorenz.edu.co>
2026-05-27 17:17:30 -0500

    crear componente de listado de actividades

---
+++ src/app/pages/actividades/actividades.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActividadService } from '../../core/services/actividad.service';
+import { Actividad } from '../../core/models/interfaces';
+@Component({
+    selector: 'app-actividades',
+    standalone: true,
+    imports: [
+        CommonModule, RouterModule, MatCardModule,
+        MatButtonModule, MatIconModule, MatSnackBarModule,
+    ],
+    templateUrl: './actividades.component.html',
+    styleUrl: './actividades.component.css'
+})
+export class ActividadesComponent implements OnInit {
+    actividades: Actividad[] = [];
+    esAdmin = false;
+    constructor(
+        private actividadService: ActividadService,
+        private snackBar: MatSnackBar
+    ) { }
+    ngOnInit() {
+        this.esAdmin = localStorage.getItem('rol') === 'administrador';
+        this.cargarActividades();
+    }
+    cargarActividades() {
+        this.actividadService.getActividades().subscribe({
+            next: (res) => this.actividades = res.actividades,
+            error: () => this.snackBar.open('Error al cargar actividades', 'Cerrar', { duration: 3000 })
+        });
+    }
+    eliminar(id: string) {
+        if (confirm('Deseas eliminar esta actividad?')) {
+            this.actividadService.eliminarActividad(id).subscribe({
+                next: () => {
+                    this.snackBar.open('Actividad eliminada', 'Cerrar', { duration: 3000 });
+                    this.cargarActividades();
+                },
+                error: () => this.snackBar.open('Error al eliminar', 'Cerrar', { duration: 3000 })
+            });
+        }
+    }
+}
